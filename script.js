38a430ab208f4f5aa7a6501215b37d79764326f2
Guilherme Guedes <gguedesprofissional@gmail.com>
2025-03-28 10:12:35 -0300

    adicionado tempo para desvirar a carta

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let qtdDeCartas = Number(prompt('Com quantas cartas deseja jogar? (Escolha entre
 let cartas = []
 let contagem = 1
 
+let primeiraCarta;
+let segundaCarta;
+
 while(qtdDeCartas % 2 !== 0 || qtdDeCartas > 16 || qtdDeCartas < 4) {
     qtdDeCartas = Number(prompt('Com quantas cartas deseja jogar? (Escolha entre 4 e 14, apenas numeros pares)'))    
     console.log(qtdDeCartas)
@@ -12,16 +15,28 @@ while(qtdDeCartas % 2 !== 0 || qtdDeCartas > 16 || qtdDeCartas < 4) {
 while(cartas.length < qtdDeCartas){
     cartas.push(contagem)
     cartas.sort(comparador)
-    document.querySelector('section').innerHTML += `<div class="card">
+    document.querySelector('section').innerHTML += `<div class="card-frente" onclick="virarCarta(this)">
     <img src="images/front.png" alt="">
+    <img class="verso" src="images/fiestaparrot.gif" alt="">
     </div>`
     contagem++
-
     
 }
 
 function comparador(){
     return Math.random() - 0.5;
 }
+
+
+function virarCarta(elemento){
+    elemento.classList.add('virar-carta')
+
+    setTimeout(() => { elemento.classList.remove('virar-carta')
+        
+    }, 3000);;
+    
+
+}
+
 
 console.log(cartas)
